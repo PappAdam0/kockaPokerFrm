53cfba768bd1e7f94f5cb67646c6f599b1e1d059
PappAdam0 <papp.adaam@gmail.com>
2020-11-06 13:06:31 +0100

    Add project files.

---
+++ kockaPokerFrm/Dobas.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kockaPokerFrm
+{
+    class Dobas
+    {
+        int[] kockak = new int[5];
+
+        public int[] Kockak
+        {
+            get
+            {
+                return kockak;
+            }
+        }
+        public string Eredmeny
+        {
+            get
+            {
+                return eredmeny;
+            }
+        }
+        private string eredmeny;
+
+        private int pont;
+        public int Pont
+        {
+            get
+            {
+                return pont;
+            }
+        }
+        public int Nyert { get; set; }
+        public Dobas()
+        {
+            Nyert = 0;
+        }
+        public Dobas(int k1, int k2, int k3, int k4, int k5)
+        {
+            kockak[0] = k1;
+            kockak[1] = k2;
+            kockak[2] = k3;
+            kockak[3] = k4;
+            kockak[4] = k5;
+
+            eredmeny = Erteke();
+        }
+
+        public void EgyDobas()
+        {
+            Random vel = new Random(Guid.NewGuid().GetHashCode());
+
+            for (int i = 0; i < kockak.Length; i++)
+            {
+                kockak[i] = vel.Next(1, 7);
+            }
+            eredmeny = Erteke();
+
+        }
+
+        public void Kiiras()
+        {
+            foreach (var i in kockak)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine($"-> {eredmeny}");
+
+
+        }
+
+        public string Erteke()
+        {
+            Dictionary<int, int> eredmenyek = new Dictionary<int, int>();
+            for (int i = 0; i <= 6; i++)
+            {
+                eredmenyek.Add(i, 0);
+            }
+            foreach (var i in kockak)
+            {
+                eredmenyek[i]++;
+            }
+
+            /*Dictionarybol lekérdezzük az 1 Valuenal nagyobb elemeket
+             * Első esetnél ha egy elem marad (valuet nézzük),
+             *  5 -> nagypóker
+             *  4 -> Póker
+             *  3 -> Drill
+             *  2 -> Pár
+             * Key érték mondja meg hogy hányas pl: 4-es Póker
+             * 2. Eset 2 elem marad
+             *  Value 3 és 2 -> Full
+             *  Value 2 és 2 -> 2 Pár
+             * 3. Eset nem marad egy sem
+             *  Ha key 6 == 0 kis sor vagy key 1 == 0 nagy sor
+             * Minden más esetben moslék
+            */
+
+            var result = (from e in eredmenyek
+                          where e.Value > 1
+                          orderby e.Value descending
+                          select new { Szam = e.Key, Db = e.Value }).ToList();
+            Console.WriteLine();
+
+            int darab = result.Count;
+            if (darab == 1)
+            {
+                string[] egyes = new string[] { "", "", "Pár", "Drill", "Póker", "Nagypóker" };
+
+                int[] pontok = new int[] { 0, 0, 10, 300, 600, 900 };
+                pont = pontok[result[0].Db] + result[0].Szam;
+
+                return $"{result[0].Szam} {egyes[result[0].Db]}";
+
+
+                //switch (result[0].Db)
+                //{
+                //    case 5:
+
+                //        return $"{result[0].Szam} Nagypóker";
+                //    case 4: return $"{result[0].Szam} Póker";
+                //    case 3: return $"{result[0].Szam} Drill";
+                //    case 2: return $"{result[0].Szam} Pár";
+
+                //}
+            }
+            else if (darab == 2)
+            {
+                if (result[0].Db == 3 && result[1].Db == 2)
+                {
+                    pont = 500 + (result[0].Szam * 10 + result[1].Szam);
+                    return $"{result[0].Szam}-{result[1].Szam} Full";
+                }
+                else
+                {
+                    pont = 100 + (result[0].Szam * 10 + result[1].Szam);
+                    return $"{result[1].Szam}-{result[0].Szam} Pár";
+                }
+            }
+            else
+            {
+                if (eredmenyek[6] == 0)
+                {
+                    pont = 700;
+                    return "Kissor";
+                }
+                else if (eredmenyek[1] == 0)
+                {
+                    pont = 800;
+                    return "Nagysor";
+                }
+
+            }
+
+            pont = 1;
+            return "Szemét";
+        }
+    }
+}
---
+++ kockaPokerFrm/Form1.cs
@@ -0,0 +1,93 @@
+﻿using kockaPokerFrm.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kockaPokerFrm
+{
+    public partial class frmFo : Form
+    {
+        private Dobas gep;
+        private Dobas ember;
+        private PictureBox[] gepKep;
+        private PictureBox[] emberKep;
+        public frmFo()
+        {
+            InitializeComponent();
+            gepKep = new PictureBox[] {pbGep1, pbGep2, pbGep3, pbGep4, pbGep5 };
+            emberKep = new PictureBox[] {pbEmber1, pbEmber2, pbEmber3, pbEmber4, pbEmber5 };
+            gep = new Dobas();
+            ember = new Dobas();
+            lblGepreszeredmeny.Text = "";
+            lblEmberreszeredmeny.Text = "";
+            lblGepEredmeny.Text = "";
+            lblEmberEredmeny.Text = "";
+        }
+
+        private void kepElhelyez(PictureBox pb, int szam)
+        {
+            switch (szam)
+            {
+                case 1:
+                    pb.Image = Resources.egy;
+                    break;
+                case 2:
+                    pb.Image = Resources.ketto;
+                    break;
+                case 3:
+                    pb.Image = Resources.harom;
+                    break;
+                case 4:
+                    pb.Image = Resources.negy;
+                    break;
+                case 5:
+                    pb.Image = Resources.ot;
+                    break;
+                case 6:
+                    pb.Image = Resources.hat;
+                    break;
+
+            }
+        }
+
+        private void DobasMegjelenit(Dobas d, PictureBox[] kepek)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                kepElhelyez(kepek[i], d.Kockak[i]);
+            }
+        }
+        private void btnDobas_Click(object sender, EventArgs e)
+        {
+            gep.EgyDobas();
+            ember.EgyDobas();
+            DobasMegjelenit(gep, gepKep);
+            DobasMegjelenit(ember, emberKep);
+            lblEmberreszeredmeny.Text = ember.Eredmeny;
+            lblGepreszeredmeny.Text = gep.Eredmeny;
+
+            if (gep.Pont > ember.Pont)
+            {
+                MessageBox.Show("Gép Nyert", "Eredmény", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                gep.Nyert++;
+                lblGepEredmeny.Text = gep.Nyert.ToString();
+            }
+            else if (ember.Pont > gep.Pont)
+            {
+                MessageBox.Show("Ember Nyert", "Eredmény", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ember.Nyert++;
+                lblEmberEredmeny.Text = ember.Nyert.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Döntetlen", "Eredmény", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
